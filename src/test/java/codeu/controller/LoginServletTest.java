ea8b14119fba8996ea9cb945a9e69128e34b0625
Manjil Gautam <gautammanjil@github.com>
2018-05-22 17:15:43 -0500

    Revert "Adding Prototype for Profile Page"
    This reverts commit 14b373c3bee96b63b8537fb8ab92c4b72f955457.

--- src/test/java/codeu/controller/LoginServletTest.java
+++ src/test/java/codeu/controller/LoginServletTest.java
@@ -16,7 +16,6 @@ package codeu.controller;
 
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
-
 import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
@@ -26,75 +25,74 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class LoginServletTest {
 
-    private LoginServlet loginServlet;
-    private HttpServletRequest mockRequest;
-    private HttpServletResponse mockResponse;
-    private RequestDispatcher mockRequestDispatcher;
+  private LoginServlet loginServlet;
+  private HttpServletRequest mockRequest;
+  private HttpServletResponse mockResponse;
+  private RequestDispatcher mockRequestDispatcher;
 
-    @Before
-    public void setup() {
-        loginServlet = new LoginServlet();
-        mockRequest = Mockito.mock(HttpServletRequest.class);
-        mockResponse = Mockito.mock(HttpServletResponse.class);
-        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/login.jsp"))
-                .thenReturn(mockRequestDispatcher);
-    }
+  @Before
+  public void setup() {
+    loginServlet = new LoginServlet();
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+    mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/login.jsp"))
+        .thenReturn(mockRequestDispatcher);
+  }
 
-    @Test
-    public void testDoGet() throws IOException, ServletException {
-        loginServlet.doGet(mockRequest, mockResponse);
+  @Test
+  public void testDoGet() throws IOException, ServletException {
+    loginServlet.doGet(mockRequest, mockResponse);
 
-        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-    }
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
 
-    @Test
-    public void testDoPost_NewUser() throws IOException, ServletException {
-        Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
-        Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
+  @Test
+  public void testDoPost_NewUser() throws IOException, ServletException {
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
+    Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
 
-        UserStore mockUserStore = Mockito.mock(UserStore.class);
-        Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(false);
-        loginServlet.setUserStore(mockUserStore);
+    UserStore mockUserStore = Mockito.mock(UserStore.class);
+    Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(false);
+    loginServlet.setUserStore(mockUserStore);
 
-        loginServlet.doPost(mockRequest, mockResponse);
+    loginServlet.doPost(mockRequest, mockResponse);
 
-        Mockito.verify(mockRequest).setAttribute("error", "That username was not found.");
-        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-    }
+    Mockito.verify(mockRequest).setAttribute("error", "That username was not found.");
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
 
-    @Test
-    public void testDoPost_ExistingUser() throws IOException, ServletException {
+  @Test
+  public void testDoPost_ExistingUser() throws IOException, ServletException {
 
-        User user =
-                new User(
-                        UUID.randomUUID(),
-                        "test username",
-                        "$2a$10$.e.4EEfngEXmxAO085XnYOmDntkqod0C384jOR9oagwxMnPNHaGLa",
-                        Instant.now());
+    User user =
+        new User(
+            UUID.randomUUID(),
+            "test username",
+            "$2a$10$.e.4EEfngEXmxAO085XnYOmDntkqod0C384jOR9oagwxMnPNHaGLa",
+            Instant.now());
 
-        Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
-        Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
+    Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
 
-        UserStore mockUserStore = Mockito.mock(UserStore.class);
-        Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
-        Mockito.when(mockUserStore.getUser("test username")).thenReturn(user);
-        loginServlet.setUserStore(mockUserStore);
+    UserStore mockUserStore = Mockito.mock(UserStore.class);
+    Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
+    Mockito.when(mockUserStore.getUser("test username")).thenReturn(user);
+    loginServlet.setUserStore(mockUserStore);
 
-        HttpSession mockSession = Mockito.mock(HttpSession.class);
-        Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+    HttpSession mockSession = Mockito.mock(HttpSession.class);
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
 
-        loginServlet.doPost(mockRequest, mockResponse);
+    loginServlet.doPost(mockRequest, mockResponse);
 
-        Mockito.verify(mockUserStore, Mockito.never()).addUser(Mockito.any(User.class));
-        Mockito.verify(mockSession).setAttribute("user", "test username");
-        Mockito.verify(mockResponse).sendRedirect("/conversations");
-    }
+    Mockito.verify(mockUserStore, Mockito.never()).addUser(Mockito.any(User.class));
+    Mockito.verify(mockSession).setAttribute("user", "test username");
+    Mockito.verify(mockResponse).sendRedirect("/conversations");
+  }
 }
