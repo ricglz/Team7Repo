63549d721f3804536d3a2bfaf029cf07ad59c5fd
ricglz0201 <rjgcastillo@hotmail.com>
2018-06-20 18:26:14 -0500

    WIP: Turned everything into a enum

--- src/main/java/codeu/helper/BotActions.java
+++ src/main/java/codeu/helper/BotActions.java
@@ -24,74 +24,180 @@ public class BotActions {
         SET_DESCRIPTION{
             @Override
             public void doAction(Object ... argsObjects) {
-                String description = argsObjects[0].toString();
-                setDescription(description);
+                String description = (String) argsObjects[0];
+                user.setDescription(description);
+                userStore.updateUser(user);
             }
-        }/*,
+        },
         CREATE_CONVERSATION{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                String title = (String) argsObjects[0];
+                UUID owner = user.getId();
+                Conversation conversation = new Conversation(UUID.randomUUID(), owner, title, Instant.now());
+                conversationStore.addConversation(conversation);
+                String content = "<a href=\"/chat/"+ title+ "\">"+ title + "</a> has been created !";
+                addAnswerMessageToStorage(content);
+            }
         },
         SEND_MESSAGE{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                String title = (String) argsObjects[0];
+                String content = (String) argsObjects[1];
+                Conversation conversation = conversationStore.getConversationWithTitle(title);
+                UUID id = conversation.getId();
+                UUID author = user.getId();
+                Message message = new Message(UUID.randomUUID(), id, author, content, Instant.now());
+                messageStore.addMessage(message);
+                String  answerContent= content + " sent to <a href=\"/chat/"+ title+ "\">"+ title + "</a>";
+                addAnswerMessageToStorage(answerContent);
+            }
         },
         GET_MESSAGES_FROM_CONVERSATION{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) {
+                String title = (String) argsObjects[0];
+                Conversation conversation = conversationStore.getConversationWithTitle(title);
+                UUID id = conversation.getId();
+                List<Message> messages = messageStore.getMessagesInConversation(id);
+                String  content= "Messages in the conversation <a href=\"/chat/"+ title+ "\">"+ title + "</a>";
+                addAnswerMessageToStorage(content);
+            }
+        },/*
         SET_SETTING{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
+        },*/
         NAVIGATE{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) throws IOException {
+                String location = (String) argsObjects[0];
+                if(!location.contains("/")){
+                    location = "/" + location;
+                }
+                String content;
+                if (validLocation(location)) {
+                    content = "You have been redirect to: <a href=\"/chat/"+ location+ "\">"+ location + "</a>";
+                    goTo(location);
+                }
+                else {
+                    content = "Location is invalid";
+                }
+                addAnswerMessageToStorage(content);
+            }
+        },/*
         GET_STAT{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_SETTING{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
+        },*/
         GET_MESSAGES_AT_TIME{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) {
+                Instant time = (Instant) argsObjects[0];
+                List<Message> messages = messageStore.getMessagesInTime(time);
+                String  content= "Messages within the time of " + time.toString() + " have been retrieved";
+                addAnswerMessageToStorage(content);
+            }
+        },/*
         GET_TUTORIAL{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_MY_MESSAGES{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
+        },*/
         GET_CONVERSATION_BY_CREATION_TIME{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                Instant time = (Instant) argsObjects[0];
+                List<Conversation> conversations = conversationStore.getConversationsByTime(time);
+                String  content= "Conversations within the time of " + time.toString() + " have been retrieved";
+                addAnswerMessageToStorage(content);
+            }
         },
         GET_CONVERSATION_BY_CREATOR{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                String username = (String) argsObjects[0];
+                User user = userStore.getUser(username);
+                UUID ownerId = user.getId();
+                List<Conversation> conversations = conversationStore.getConversationsByAuthor(ownerId);
+                String  content= "Conversations done by " + username + " have been retrieved";
+                addAnswerMessageToStorage(content);
+            }
         },
         GET_CONVERSATIONS{
-
-        },
+            @Override
+            public void doAction(Object ... argsObjects) {        
+                String  content= "All conversations have been retrieved";
+                addAnswerMessageToStorage(content);
+            }
+        },/*
         GET_STATS{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_SETTINGS{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         NAVIGATE_TO_CONVERSATION{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_CONVERSATION_SUMMARY{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_MESSAGES_LIKE_KEYWORD{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_CONVESATION_WITH_CONTENT_LIKE_KEYWORD{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_CONVERSATION_LIKE_KEYWORD{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         },
         GET_CONVERSATION_WITH_UNREAD_MESSAGES{
-
+            @Override
+            public void doAction(Object ... argsObjects) {
+                
+            }
         }*/;
-        public abstract void doAction(Object ... argsObjects);
+        public abstract void doAction(Object ... argsObjects) throws IOException;
     }
 
     private static ActivityStore activityStore;
@@ -139,88 +245,20 @@ public class BotActions {
         this.userStore = userStore;
     }
 
-    public void addAnswerMessageToStorage(String content) {
+    private static void addAnswerMessageToStorage(String content) {
         Message message = new Message(UUID.randomUUID(), conversation.getId(), 
                                         user.getId(), content, Instant.now());
         messageStore.addMessage(message);
     }
 
-    public static void setDescription(String description) {
-        user.setDescription(description);
-        userStore.updateUser(user);
-    }
-
-    private boolean validLocation(String location) {
+    private static boolean validLocation(String location) {
         List<String> valid_locations = Lists.newArrayList("/register", "/login", "/about.jsp", "/activity",
                                                         "/admin", "/profile", "/conversations");
         return valid_locations.contains(location);
     }
 
-    public void goTo(String location) throws IOException {
-        if(!location.contains("/")){
-            location = "/" + location;
-        }
-        if (validLocation(location)) {
-            response.sendRedirect(location);   
-        }
-        String content = "You have been redirect to: <a href=\"/chat/"+ location+ "\">"+ location + "</a>";
-        addAnswerMessageToStorage(content);
-    }
-
-    public void createConversation(String title) throws IOException {
-        UUID owner = user.getId();
-        Conversation conversation = new Conversation(UUID.randomUUID(), owner, title, Instant.now());
-        conversationStore.addConversation(conversation);
-        String content = "<a href=\"/chat/"+ title+ "\">"+ title + "</a> has been created !";
-        addAnswerMessageToStorage(content);
-    }
-
-    public void sendMessage(String content, String title) throws IOException {
-        Conversation conversation = conversationStore.getConversationWithTitle(title);
-        UUID id = conversation.getId();
-        UUID author = user.getId();
-        Message message = new Message(UUID.randomUUID(), id, author, content, Instant.now());
-        messageStore.addMessage(message);
-        String  answerContent= content + " sent to <a href=\"/chat/"+ title+ "\">"+ title + "</a>";
-        addAnswerMessageToStorage(answerContent);
-    }
-
-    public List<Message> getMessages(Instant time) {
-        List<Message> messages = messageStore.getMessagesInTime(time);
-        String  content= "Messages within the time of " + time.toString() + " have been retrieved";
-        addAnswerMessageToStorage(content);
-        return messages;
-    }
-
-    public List<Message> getMessages(String title) {
-        Conversation conversation = conversationStore.getConversationWithTitle(title);
-        UUID id = conversation.getId();
-        List<Message> messages = messageStore.getMessagesInConversation(id);
-        String  content= "Messages in the conversation <a href=\"/chat/"+ title+ "\">"+ title + "</a>";
-        addAnswerMessageToStorage(content);
-        return messages;
-    }
-
-    public List<Conversation> getConversations(Instant time) {
-        List<Conversation> conversations = conversationStore.getConversationsByTime(time);
-        String  content= "Conversations within the time of " + time.toString() + " have been retrieved";
-        addAnswerMessageToStorage(content);
-        return conversations;
-    }
-
-    public List<Conversation> getConversations(String username) {
-        User user = userStore.getUser(username);
-        UUID ownerId = user.getId();
-        List<Conversation> conversations = conversationStore.getConversationsByAuthor(ownerId);
-        String  content= "Conversations done by " + username + " have been retrieved";
-        addAnswerMessageToStorage(content);
-        return conversations;
-    }
-
-    public List<Conversation> getConversations() {
-        String  content= "All conversations have been retrieved";
-        addAnswerMessageToStorage(content);
-        return conversationStore.getAllConversations();
+    private static void goTo(String location) throws IOException {
+        response.sendRedirect(location);
     }
 
 }
