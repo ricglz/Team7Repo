6869c2aa8f6c728336517759df420c9a0a2401cd
Kirielle Singarajah <kiriellesingarajah@gmail.com>
2018-06-28 11:41:17 -0500

    Fix deleted implementations

--- src/main/java/codeu/bot/ActionMatcher.java
+++ src/main/java/codeu/bot/ActionMatcher.java
@@ -1,4 +1,4 @@
-// package codeu.bot;
+package codeu.bot;
 
 import edu.stanford.nlp.coref.data.CorefChain;
 import edu.stanford.nlp.ling.*;
@@ -54,7 +54,7 @@ public class ActionMatcher {
     public static LevenshteinDistance distance;
 
     public ActionMatcher(String input) {
-        this.input = input.trim();
+        this.input = input;
         distance = new LevenshteinDistance();
     }
 
@@ -151,150 +151,86 @@ public class ActionMatcher {
         System.out.println(commandTokens);
         System.out.println(commandPOS);
         System.out.println(nerTags);
-        System.out.println("HERE"+timexAnnotationsAll);
+        System.out.println(timexAnnotationsAll);
 
-        // GET...
-        int getOrFindOrDisplayorShoworGiveIndex = ac.getKeywordIndex(new String[] {"get","find","display","show","give"}, commandTokensLemmas);
-        if (getOrFindOrDisplayorShoworGiveIndex != -1 && 
-            commandPOS.get(getOrFindOrDisplayorShoworGiveIndex).contains("VB")) {
-            // Going to be getting something
-            // Have a lot of methods where we get something so
-            // this block of code is going to branch a lot
+        ////////////////////////////////////////////////////////////////////////
+        // SETTING/UPDATING/CHANGING DESCRIPTION/SETTINGS]
+        int setOrUpdateOrChangeIndex = getKeywordIndex(new String[] {"set","update","change"}, commandTokensLemmas);
+        if (setOrUpdateOrChangeIndex != -1 && 
+            commandPOS.get(setOrUpdateOrChangeIndex).contains("VB")) {
+            // Going to be setting/updating/changing description or setting
 
-            // Getting the lemma so we can ignore pluralizations
-
-            IndexedWord verbOfInterest = new IndexedWord(commandTokens.get(getOrFindOrDisplayorShoworGiveIndex));
-            // Set<IndexedWord> childrenWithReln = dependencyParse.getChildrenWithReln(verbOfInterest);
-            // Find object we're getting
-            Set<IndexedWord> directObjects = dependencyParse.getChildrenWithReln(verbOfInterest,UniversalEnglishGrammaticalRelations.DIRECT_OBJECT);
-            String directObject = "";
-            if (!directObjects.isEmpty() && directObjects.size() == 1) {
-                // Is it even possible to have multiple direct objects
-                // from a single verb?
-                // Going to go with no...
-
-                directObject = (new ArrayList<>(directObjects)).get(0).lemma();
-            } else {
-                // RAISE ERROR, SORRY I'M NOT SURE WHAT YOU'RE LOOKING FOR.
+            // If setting/updating/changing the description, must use keyword description
+            int descIndex = commandTokensLemmas.indexOf("description");
+            String description = "";
+            if (descIndex != -1 && commandPOS.get(descIndex).contains("NN")) {
+                // Find set description parameter
+                Pattern pattern = Pattern.compile("\"([^\"]*)\"");
+                Matcher matcher = pattern.matcher(input);
+                if (matcher.find()) {
+                    description = matcher.group(1);
+                }
+                System.out.printf("Setting description to %s.",description);
+                BotActions.action.SET_DESCRIPTION.doAction(description);
                 return;
             }
 
-            switch (directObject) {
-                case "message": {
-                    // Getting messages
-
-                    // GET_MY_MESSAGES
-                    if (commandTokensLemmas.contains("my") || commandTokensLemmas.contains("I")) {
-                        System.out.printf("Showing your messages.");
-                        // BotActions.action.GET_MY_MESSAGES.doAction();
-                        return;
-                    }
-
-                    // GET_MESSAGES_BY_CREATION_TIME
-                    if (!timexAnnotationsAll.isEmpty() && timexAnnotationsAll.size() == 1) {
-                        CoreMap timexAnnotation = timexAnnotationsAll.get(0);
-                        Temporal temporal = timexAnnotation.get(TimeExpression.Annotation.class).getTemporal();
-                        // BotActions.action.GET_MESSAGES_BY_CREATION_TIME.doAction(temporal);
-                        System.out.printf("Getting messages based on time criteria: %s.",temporal.toString());
-                        return;
-                    }
-
-                    // GET_MESSAGES_LIKE_KEYWORD
-                    int containOrAboutIndex = ac.getKeywordIndex(new String[] {"contain","about"}, commandTokensLemmas);
-                    if (containOrAboutIndex != -1) {
-                        String keyword = "";
-                        Pattern keywordPattern = Pattern.compile("<([^\"]*)>");
-                        Matcher keywordMatcher = keywordPattern.matcher(input);
-                        if (keywordMatcher.find()) {
-                            keyword = keywordMatcher.group(1);
-                        } else {
-                            // RAISE SORRY IDK
-                            return;
-                        }
-                        
-                        if (!keyword.equals("")) {
-                            // BotActions.action.GET_MESSAGES_LIKE_KEYWORD.doAction(keyword);
-                            System.out.printf("Getting messages like keyword \"%s\".",keyword);
-                            return;
-                        }
-                    }
-
-                    // GET_MESSAGES_FROM_CONVERSATION
-                    int fromOrInIndex = ac.getKeywordIndex(new String[] {"in","from"}, commandTokensLemmas);
-                    if (fromOrInIndex != -1) {
-                        String conversationTitle = ac.findFuzzyMatch(conversationTitles, 3);
-                        if (!conversationTitle.equals("")) {
-                            // BotActions.action.GET_MESSAGES_FROM_CONVERSATION.doAction(conversationTitle);
-                            System.out.printf("Getting messsages from %s.",conversationTitle);
-                            return;
-                        }
-                    }
-                    
-                }
-                case "conversation": {
-                    // Getting conversations
-                }
-            }
+            // TODO: Settings implementation
         }
-        return;
-    }
 
 
-    public static void main(String[] args) {
-        // Options for how this interacts with the action executor:
-        // 1. Return an ActivityType implementing object
-        // which has been instantiated with the appropriate parameters
-        // 2. Access the enum type and call the doAction method
-        // with Object parameter? Q: Is this an array?
+        ////////////////////////////////////////////////////////////////////////
+        // CREATE_CONVERSATION, params: String conversationTitle
+        int createOrMakeIndex = getKeywordIndex(new String[] {"create","make"}, commandTokensLemmas);
+        if (createOrMakeIndex != -1 && 
+            commandPOS.get(createOrMakeIndex).contains("VB")) {
+            // Going to be creating something
+            // Right now we can only create conversations so don't need to
+            // look for conversation keyword
 
-        String input = args[0];
-        ActionMatcher ac = new ActionMatcher(input);
-
-        HashSet<String> conversationTitles = new HashSet<String>();
-
-        conversationTitles.add("Untitled");
-        // LevenshteinDistance distance = new LevenshteinDistance();
-
-        Properties properties = new Properties();
-        properties.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,depparse");
-        properties.setProperty("sutime.markTimeRanges", "true");
-
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(properties);
-        pipeline.addAnnotator(new TimeAnnotator("sutime", properties));
-
-
-        CoreDocument document = new CoreDocument(input);
-        // document.annotation().set(CoreAnnotations.DocDateAnnotation.class,"2018-06-28");
-        document.annotation().set(CoreAnnotations.DocDateAnnotation.class,(new Date()).toString());
-
-        pipeline.annotate(document);
-
-        if (document.sentences().size() != 1) {
-            System.out.println("Raise error, one command per message please.");
+            String conversationTitle = "";
+            Pattern pattern = Pattern.compile("<([^\"]*)>");
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                conversationTitle = matcher.group(1);
+                System.out.println("got here");
+            }
+            System.out.printf("Creating conversation %s.",conversationTitle);
+            BotActions.action.CREATE_CONVERSATION.doAction(conversationTitle);
             return;
         }
 
 
-        // tokens returns a CoreLabel ArrayList
-        // posTags returns a String ArrayList
-        CoreSentence command = document.sentences().get(0);
-        ArrayList<CoreLabel> commandTokens = (ArrayList<CoreLabel>) command.tokens();
-        ArrayList<String> commandTokensLemmas = new ArrayList<String>();
-        SemanticGraph dependencyParse = command.dependencyParse();
-        ArrayList<String> nerTags = (ArrayList<String>) command.nerTags();
-        List<CoreMap> timexAnnotationsAll = document.annotation().get(TimeAnnotations.TimexAnnotations.class);
+        ////////////////////////////////////////////////////////////////////////
+        // SEND_MESSAGE, params: String message, String conversationTitle
+        int sendIndex = commandTokensLemmas.indexOf("send");
+        if (sendIndex != -1 && 
+            commandPOS.get(sendIndex).contains("VB")) {
+            // Going to be sending something
+            // Same logic for create, know we're looking for a message and conversation
 
-        for (CoreLabel label : commandTokens) {
-            commandTokensLemmas.add(label.value().toLowerCase());
+            String message = "";
+            Pattern messagePattern = Pattern.compile("\"([^\"]*)\"");
+            Matcher messageMatcher = messagePattern.matcher(input);
+            if (messageMatcher.find()) {
+                message = messageMatcher.group(1);
+            }
+
+            // Deprecated, attempting fuzzy matching of conversation titles
+            // Pattern conversationTitlePattern = Pattern.compile("<([^\"]*)>");
+            // Matcher conversationTitleMatcher = conversationTitlePattern.matcher(input);
+            // if (conversationTitleMatcher.find()) {
+            //     conversationTitle = conversationTitleMatcher.group(1);
+            // }
+
+            String conversationTitle = findFuzzyMatch(conversationTitles, 5);
+            System.out.printf("Sending message \"%s\" in conversation <%s>.",message,conversationTitle);
+            BotActions.action.SEND_MESSAGE.doAction(message,conversationTitle);
+            return;
         }
 
-        ArrayList<String> commandPOS = (ArrayList<String>) command.posTags();
-
-        System.out.println(dependencyParse);
-        System.out.println(commandTokens);
-        System.out.println(commandPOS);
-        System.out.println(nerTags);
-        System.out.println("HERE"+timexAnnotationsAll);
+        ////////////////////////////////////////////////////////////////////////
+        // GETTING THINGS
 
         // GET...
         int getOrFindOrDisplayorShoworGiveIndex = ac.getKeywordIndex(new String[] {"get","find","display","show","give"}, commandTokensLemmas);
@@ -381,4 +317,5 @@ public class ActionMatcher {
         }
         return;
     }
+
 }
